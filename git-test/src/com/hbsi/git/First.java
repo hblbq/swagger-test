7e34d9723c9e5753016aa20fb56ede1de398c48d
lbq <1445719095@qq.com>
2020-08-03 23:31:14 +0800

    1111111111111111

---
+++ git-test/src/com/hbsi/git/First.java
@@ -0,0 +1,16 @@
+package com.hbsi.git;
+
+/**
+ * @author lbq
+ * @date 2020 08  23:15
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+
+    }
+}
